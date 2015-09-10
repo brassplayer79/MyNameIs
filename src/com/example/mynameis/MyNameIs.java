95893a7c89fd0c02f9eb79fa5b1f02f290275d85
brassplayer79 <afree1@umbc.edu>
2015-09-10 14:16:38 -0400

    MyNameIs.java

---
+++ src/com/example/mynameis/MyNameIs.java
@@ -0,0 +1,10 @@
+package com.example.mynameis;
+
+/**
+ * Created by Amy L. Freeland on 9/9/15.
+ */
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is Amy.");
+    }
+}
